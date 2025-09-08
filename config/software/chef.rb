dafcbc0a0d10497ff402a57f6f6e07403480fe5a
Peleg Bar <pelegb@cb4.com>
2025-09-09 00:38:46 +0300

    CAPP-29848 Skip building ruby-shadow

--- config/software/chef.rb
+++ config/software/chef.rb
@@ -53,12 +53,12 @@ build do
   # native extensions for pry-byebug so excluding for now
   excluded_groups = %w{server docgen maintenance pry travis integration ci}
   excluded_groups << "ruby_prof" if aix?
-  excluded_groups << "ruby_shadow" if aix?
+  excluded_groups << "ruby_shadow"
   # ruby_shadow 2.5.0 breaks with ruby 3, but 2.5.1 works with ruby 3.2
   # Force _GNU_SOURCE so shadow.h prototypes (getspent, fgetspent, etc.) are visible on glibc 2.17
-  env["CFLAGS"] = "#{env["CFLAGS"]} -D_GNU_SOURCE"
-
-  gem "install ruby-shadow --version 2.5.1 -- --with-cflags='-D_GNU_SOURCE'", env: env
+#   env["CFLAGS"] = "#{env["CFLAGS"]} -D_GNU_SOURCE"
+#
+#   gem "install ruby-shadow --version 2.5.1 -- --with-cflags='-D_GNU_SOURCE'", env: env
   bundle "update --bundler"
   # install the whole bundle first
   bundle "install --verbose --without #{excluded_groups.join(' ')}", env: env
