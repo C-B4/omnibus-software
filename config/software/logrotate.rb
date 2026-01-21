25bee176ff7c68402a0019f7610edf06ecc28399
Peleg Bar <pelegb@cb4.com>
2026-01-21 11:42:51 +0200

    CAPP-29848 ruby

--- config/software/logrotate.rb
+++ config/software/logrotate.rb
@@ -15,7 +15,7 @@
 #
 
 name "logrotate"
-default_version "3.9.2"
+default_version "3.18.1"
 
 license "GPL-2.0"
 license_file "COPYING"
@@ -25,6 +25,7 @@ dependency "popt"
 
 source url: "https://github.com/logrotate/logrotate/archive/#{version}.tar.gz"
 
+version("3.18.1") { source md5: "2aa6338b037e04548c8bffc73da24d7c"}
 version("3.9.2") { source md5: "584bca013dcceeb23b06b27d6d0342fb" }
 version("3.8.9") { source md5: "e6da1f1b91d1f202d26caaf864aa0d71" }
 
@@ -33,13 +34,18 @@ relative_path "logrotate-#{version}"
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
+  # Disable warnings-as-errors for newer GCC compatibility
+  # GCC 11+ has stricter warnings that flag issues in logrotate 3.9.2
+  env["CFLAGS"] << " -Wno-error=misleading-indentation -Wno-error=stringop-overflow"
+
   # Run autogen.sh to generate configure script
   command "./autogen.sh", env: env
 
-  # Configure with proper prefix
+  # Configure with proper prefix and disable SELinux (not needed for embedded use)
   configure_options = [
     "--prefix=#{install_dir}/embedded",
-    "--with-popt=#{install_dir}/embedded"
+    "--with-popt=#{install_dir}/embedded",
+    "--without-selinux"
   ]
 
   configure(*configure_options, env: env)
