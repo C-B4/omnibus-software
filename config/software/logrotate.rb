41d7773680c38b93c422cbde67d96890b0aa3885
Peleg Bar <pelegb@cb4.com>
2026-01-21 10:11:59 +0200

    CAPP-29848 ruby

--- config/software/logrotate.rb
+++ config/software/logrotate.rb
@@ -15,7 +15,7 @@
 #
 
 name "logrotate"
-default_version "3.21.0"
+default_version "3.9.2"
 
 license "GPL-2.0"
 license_file "COPYING"
@@ -24,41 +24,26 @@ skip_transitive_dependency_licensing true
 dependency "popt"
 
 source url: "https://github.com/logrotate/logrotate/archive/#{version}.tar.gz"
-internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{version}.tar.gz",
-                authorization: "X-JFrog-Art-Api:#{ENV["ARTIFACTORY_TOKEN"]}"
 
-# versions_list: https://github.com/logrotate/logrotate/tags filter=*.tar.gz
-
-version("3.21.0") { source sha256: "7f58d6ab7e4eab3403528a88d3747a91b03e83e866a8fb966551016e0df527bb" }
-version("3.20.1") { source sha256: "f37458dee3b4adab6719767ad4b93ff9ec8948755d1148b76f7f4c2c68d3e457" }
-version("3.19.0") { source sha256: "7de1796cb99ce4ed21770b5dae0b4e6f81de0b4df310a58a1617d8061b1e0930" }
-version("3.18.1") { source sha256: "18e9c9b85dd185e79f097f4e7982bc5b8c137300756a7878e8fa24731f2f8e21" }
-version("3.9.2")  { source sha256: "2de00c65e23fa9d7909cae6594e550b9abe9a7eb1553669ddeaca92d30f97009" }
+version("3.9.2") { source md5: "584bca013dcceeb23b06b27d6d0342fb" }
+version("3.8.9") { source md5: "e6da1f1b91d1f202d26caaf864aa0d71" }
 
 relative_path "logrotate-#{version}"
 
 build do
-  env = with_standard_compiler_flags(with_embedded_path).merge(
-    # Patch allows this to be set manually
-    "BASEDIR" => "#{install_dir}/embedded"
-  )
+  env = with_standard_compiler_flags(with_embedded_path)
 
-  # These EXTRA_* vars allow us to append to the Makefile's hardcoded LDFLAGS
-  # and CFLAGS
-  env["EXTRA_LDFLAGS"] = env["LDFLAGS"]
-  env["EXTRA_CFLAGS"]  = env["CFLAGS"]
+  # Run autogen.sh to generate configure script
+  command "./autogen.sh", env: env
 
-  if version == "3.9.2"
-    patch source: "logrotate_basedir_override.patch", plevel: 0, env: env
-  else
-    command   "autoreconf -fiv", env: env
-    command   "./configure --prefix=#{install_dir}/embedded", env: env
-  end
+  # Configure with proper prefix
+  configure_options = [
+    "--prefix=#{install_dir}/embedded",
+    "--with-popt=#{install_dir}/embedded"
+  ]
+
+  configure(*configure_options, env: env)
 
   make "-j #{workers}", env: env
-
-  # Yes, this is horrible. Due to how the makefile is structured, we need to
-  # specify PREFIX, *but not BASEDIR* in order to get this installed into
-  # +"#{install_dir}/embedded/sbin"+
-  make "install", env: { "PREFIX" => "#{install_dir}/embedded" }
+  make "install", env: env
 end
