34fd75053f45bf2bf5de7f3ffacb8e2b537b97e9
Peleg Bar <pelegb@cb4.com>
2026-01-25 15:42:55 +0200

    CAPP-29848 ruby

--- config/software/python.rb
+++ config/software/python.rb
@@ -59,4 +59,19 @@ build do
 
   make env: env
   make "install", env: env
+    # There exists no configure flag to tell Python to not compile readline
+  delete "#{install_dir}/embedded/lib/python#{major_version}.#{minor_version}/lib-dynload/readline.*"
+
+    # Ditto for sqlite3
+  delete "#{install_dir}/embedded/lib/python#{major_version}.#{minor_version}/lib-dynload/_sqlite3.*"
+  delete "#{install_dir}/embedded/lib/python#{major_version}.#{minor_version}/sqlite3/"
+  
+    # Remove unused extension which is known to make healthchecks fail on CentOS 6
+  delete "#{install_dir}/embedded/lib/python#{major_version}.#{minor_version}/lib-dynload/_bsddb.*"
+  
+    # Remove sqlite3 libraries, if you want to include sqlite, create a new def
+    # in your software project and build it explicitly. This removes the adapter
+    # library from python, which links incorrectly to a system library. Adding
+    # your own sqlite definition will fix this.
+  delete "#{install_dir}/embedded/lib/python#{major_version}.#{minor_version}/lib-dynload/_sqlite3.*"
 end
