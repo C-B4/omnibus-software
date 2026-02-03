cb8386a15c431b87d7b65dd13b84b448fd193022
Peleg Bar <pelegb@cb4.com>
2026-02-04 00:38:47 +0200

    CAPP-29848-ruby-3.1.3

--- config/software/chef.rb
+++ config/software/chef.rb
@@ -14,12 +14,15 @@
 # limitations under the License.
 #
 name "chef"
-default_version "v17.10.3"
+default_version "v17.2.29"
+
 license "Apache-2.0"
 license_file "LICENSE"
+
 # Grab accompanying notice file.
 # So that Open4/deep_merge/diff-lcs disclaimers are present in Omnibus LICENSES tree.
 license_file "NOTICE"
+
 # For the specific super-special version "local_source", build the source from
 # the local git checkout. This is what you'd want to occur by default if you
 # just ran omnibus build locally.
@@ -34,52 +37,59 @@ version("local_source") do
          # what a github based build will see.
          options: { exclude: [ "omnibus/vendor" ] }
 end
+
 # For any version other than "local_source", fetch from github.
 # This is the behavior the transitive omnibus software deps such as chef-dk
 # expect.
 if version != "local_source"
   source git: "https://github.com/chef/chef.git"
 end
+
 relative_path "chef"
+
 dependency "ruby"
 dependency "rubygems"
 dependency "bundler"
 dependency "ohai"
 dependency "appbundler"
 dependency "libarchive" # for archive resource
+
 build do
   env = with_standard_compiler_flags(with_embedded_path)
 
-  # Remove ruby-shadow from Gemfile completely
-  command "sed -i '/ruby-shadow/d' Gemfile", env: env
-  command "sed -i '/ruby-shadow/d' Gemfile.lock", env: env if File.exist?("Gemfile.lock")
-
+  # compiled ruby on windows 2k8R2 x86 is having issues compiling
+  # native extensions for pry-byebug so excluding for now
   excluded_groups = %w{server docgen maintenance pry travis integration ci}
   excluded_groups << "ruby_prof" if aix?
+  excluded_groups << "ruby_shadow" if aix?
+# ruby_shadow 2.5.0 breaks with ruby 3
+#  gem "install ruby-shadow -v '2.5.1'", env: env
 
   bundle "update --bundler"
-  bundle "config set --local without '#{excluded_groups.join(' ')}'", env: env
-  bundle "install --verbose", env: env
 
-  # Clean up pre-installed gems that cause dependency resolution conflicts
-  # The ohai dependency installs these, but rake install needs a clean slate
-  gem "uninstall -aIx chef-utils chef-config ohai --ignore-dependencies || true", env: env
+  # install the whole bundle first
+  bundle "install --verbose --without #{excluded_groups.join(' ')}", env: env
 
   # use the rake install task to build/install chef-config
   bundle "exec rake install", env: env
+
   gemspec_name = windows? ? "chef-universal-mingw32.gemspec" : "chef.gemspec"
+
   # This step will build native components as needed - the event log dll is
   # generated as part of this step.  This is why we need devkit.
   gem "build #{gemspec_name}", env: env
+
   # ensure we put the gems in the right place to get picked up by the publish scripts
   delete "pkg"
   mkdir "pkg"
   copy "chef*.gem", "pkg"
+
   # Always deploy the powershell modules in the correct place.
   if windows?
     mkdir "#{install_dir}/modules/chef"
     copy "distro/powershell/chef/*", "#{install_dir}/modules/chef"
   end
+
   appbundle "chef", env: env
   # appbundle "ohai", env: env
 end
