da584b9f19b980a9c79aa386bf3c1faa101af571
Peleg Bar <pelegb@cb4.com>
2026-09-06 10:24:49 +0300

    CAPP-38840 Fix HTTPS to HTTP redirection in makedepend.rb

--- config/software/makedepend.rb
+++ config/software/makedepend.rb
@@ -21,7 +21,7 @@ license "MIT"
 license_file "COPYING"
 skip_transitive_dependency_licensing true
 
-source url: "https://www.x.org/releases/individual/util/makedepend-1.0.5.tar.gz",
+source url: "https://www.x.org/archive/individual/util/makedepend-1.0.5.tar.gz",
        md5: "efb2d7c7e22840947863efaedc175747"
 
 relative_path "makedepend-1.0.5"
